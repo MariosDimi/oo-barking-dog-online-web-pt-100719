246a126a24b24962b074ed19e25d084e17c2ffe6
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-15 00:38:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-  def name
+  def name =(dog_name)
+    @this_dog_name = dog_name
+    
+    def name
+      @this_dog_name
+  end
+  def bark
+    puts "woof!"
+  end
 end
+
+
+
+snoopy=Dog.new 
+snoopy.name = "Snoopy"
+puts snoopy.name
+snoopy.bark
